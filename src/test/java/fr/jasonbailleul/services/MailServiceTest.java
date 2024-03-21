59ced023db69a8e4696dbeb332737362f3b4b3cf
djadj <jasonbailleul59@laposte.net>
2024-03-21 14:18:42 +0100

    first commit

---
+++ src/test/java/fr/jasonbailleul/services/MailServiceTest.java
@@ -0,0 +1,12 @@
+package fr.jasonbailleul.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailServiceTest {
+
+    @Test
+    void sendMail() {
+    }
+}
